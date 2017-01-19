4b391269a88d3d54faa9665f4c4aca50023d7439
Jason Gauci <jjg@fb.com>
2017-01-18 23:59:15 -0800

    Fix bug with SocketHandler and nonblocking sockets.

--- src/SocketHandler.cpp
+++ src/SocketHandler.cpp
@@ -6,8 +6,14 @@ void SocketHandler::readAll(int fd, void* buf, size_t count) {
   while (pos < count) {
     ssize_t bytesRead = read(fd, ((char*)buf) + pos, count - pos);
     if (bytesRead < 0) {
-      VLOG(1) << "Failed a call to readAll: " << strerror(errno);
-      throw std::runtime_error("Failed a call to readAll");
+      if (errno == EAGAIN ||
+          errno == EWOULDBLOCK) {
+        // This is fine, just keep retrying at 10hz
+        usleep(1000 * 100);
+      } else {
+        VLOG(1) << "Failed a call to readAll: " << strerror(errno);
+        throw std::runtime_error("Failed a call to readAll");
+      }
     }
     pos += bytesRead;
   }
@@ -18,8 +24,14 @@ void SocketHandler::writeAll(int fd, const void* buf, size_t count) {
   while (pos < count) {
     ssize_t bytesWritten = write(fd, ((const char*)buf) + pos, count - pos);
     if (bytesWritten < 0) {
-      VLOG(1) << "Failed a call to writeAll: " << strerror(errno);
-      throw std::runtime_error("Failed a call to writeAll");
+      if (errno == EAGAIN ||
+          errno == EWOULDBLOCK) {
+        // This is fine, just keep retrying at 10hz
+        usleep(1000 * 100);
+      } else {
+        VLOG(1) << "Failed a call to writeAll: " << strerror(errno);
+        throw std::runtime_error("Failed a call to writeAll");
+      }
     }
     pos += bytesWritten;
   }
