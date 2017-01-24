4f6b9b8b19abb6bfc1edd896cca00ac0b80a4973
Jason Gauci <jjg@fb.com>
2017-01-24 11:42:28 -0800

    Launcher now has correct case for binaries.  Fix strange shutdown issue on OS/X

--- src/UnixSocketHandler.cpp
+++ src/UnixSocketHandler.cpp
@@ -262,8 +262,11 @@ void UnixSocketHandler::close(int fd) {
   VLOG(1) << "Shutting down connection: " << fd << endl;
   int rc = ::shutdown(fd, SHUT_RDWR);
   if (rc == -1) {
-    if (errno == ENOTCONN) {
-      // This is harmless
+    if (errno == ENOTCONN || errno == EADDRNOTAVAIL) {
+      // Shutdown is failing on OS/X with errno (49): Can't assign requested address
+      // Possibly an OS bug but I don't think it's necessary anyways.
+
+      // ENOTCONN is harmless
     } else {
       FATAL_FAIL(rc);
     }
