345fb719a991aa30e06bc719a4c5c208ab3d146c
Jason Gauci <jgmath2000@protonmail.com>
2018-05-07 11:54:30 -0700

    Revert "start etserver in interactive mode"
    This reverts commit 24ec7ea00b11105d2625d313846cc7c24f709b38.

--- terminal/SshSetupHandler.cpp
+++ terminal/SshSetupHandler.cpp
@@ -66,15 +66,13 @@ string SshSetupHandler::SetupSsh(string user, string host, string host_alias,
     dup2(link_client[1], 1);
     close(link_client[0]);
     close(link_client[1]);
-    // run the command in interactive mode
-    SSH_SCRIPT_DST = "$SHELL -lic \'" + SSH_SCRIPT_DST + "\'";
     if (!jumphost.empty()) {
       execlp("ssh", "ssh", "-J", (SSH_USER_PREFIX + jumphost).c_str(),
              (SSH_USER_PREFIX + host_alias).c_str(), (SSH_SCRIPT_DST).c_str(),
              NULL);
     } else {
       execlp("ssh", "ssh", (SSH_USER_PREFIX + host_alias).c_str(),
-             SSH_SCRIPT_DST.c_str(), NULL);
+             (SSH_SCRIPT_DST).c_str(), NULL);
     }
 
     LOG(INFO) << "execl error";
@@ -131,9 +129,7 @@ string SshSetupHandler::SetupSsh(string user, string host, string host_alias,
         cmdoptions +=
             " --jump --dsthost=" + host + " --dstport=" + to_string(port);
         string SSH_SCRIPT_JUMP = SSH_SCRIPT_PREFIX + cmdoptions + ";true";
-	// start command in interactive mode
-        SSH_SCRIPT_JUMP = "$SHELL -lic \'" + SSH_SCRIPT_JUMP+ "\'";
-        execlp("ssh", "ssh", jumphost.c_str(), SSH_SCRIPT_JUMP.c_str(), NULL);
+        execlp("ssh", "ssh", jumphost.c_str(), (SSH_SCRIPT_JUMP).c_str(), NULL);
       } else {
         close(link_jump[1]);
         wait(NULL);
