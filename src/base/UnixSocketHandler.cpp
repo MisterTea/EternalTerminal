dcae3500fc2a0e4f37d127f0cdf86cdcfd22e55c
Jason Gauci <jjg@fb.com>
2018-06-04 16:23:51 -0700

    Call shutdown upon closing a connection

--- src/base/UnixSocketHandler.cpp
+++ src/base/UnixSocketHandler.cpp
@@ -363,22 +363,10 @@ void UnixSocketHandler::close(int fd) {
     return;
   }
   activeSockets.erase(activeSockets.find(fd));
-#if 0
   // Shutting down connection before closing to prevent the server
   // from closing it.
   VLOG(1) << "Shutting down connection: " << fd;
-  int rc = ::shutdown(fd, SHUT_RDWR);
-  if (rc == -1) {
-    if (errno == ENOTCONN || errno == EADDRNOTAVAIL) {
-      // Shutdown is failing on OS/X with errno (49): Can't assign requested address
-      // Possibly an OS bug but I don't think it's necessary anyways.
-
-      // ENOTCONN is harmless
-    } else {
-      FATAL_FAIL(rc);
-    }
-  }
-#endif
+  FATAL_FAIL(::shutdown(fd, SHUT_RDWR));
   VLOG(1) << "Closing connection: " << fd;
   FATAL_FAIL(::close(fd));
 }
