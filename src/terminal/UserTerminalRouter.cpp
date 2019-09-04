145ed8722bc6bc7aadad7b338431f697f31d86fb
Jason Gauci <jjg@fb.com>
2019-09-04 14:26:28 -0700

    allow other users to talk to the terminal router

--- src/terminal/UserTerminalRouter.cpp
+++ src/terminal/UserTerminalRouter.cpp
@@ -8,6 +8,10 @@ UserTerminalRouter::UserTerminalRouter(
     const SocketEndpoint &_routerEndpoint)
     : socketHandler(_socketHandler) {
   serverFd = *(socketHandler->listen(_routerEndpoint).begin());
+  FATAL_FAIL(::chown(_routerEndpoint.name().c_str(), getuid(), getgid()));
+  FATAL_FAIL(::chmod(_routerEndpoint.name().c_str(),
+                     S_IRUSR | S_IWUSR | S_IXUSR | S_IRGRP | S_IWGRP | S_IXGRP |
+                         S_IROTH | S_IWOTH | S_IXOTH));
 }
 
 IdKeyPair UserTerminalRouter::acceptNewConnection() {
