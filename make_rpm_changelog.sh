649366eda64c9a7c7def911386ad128afa40519f
Jason Gauci <jgmath2000@gmail.com>
2021-11-03 13:19:22 -0700

    6.1.9 changelog

--- make_rpm_changelog.sh
+++ make_rpm_changelog.sh
@@ -1,1 +1,1 @@
-git log --format="* %cd %aN%n- (%h) %s%d%n" --date=local | gsed -r 's/[0-9]+:[0-9]+:[0-9]+ //'
+git log --format="* %cd %aN%n- (%h) %s%d%n" --date=local | sed -r 's/[0-9]+:[0-9]+:[0-9]+ //'
