98eed5ba771fe18c95dac169502f7f41fd21c43b
Jason Gauci <jgmath2000@gmail.com>
2023-01-19 20:36:59 -0600

    Update submodule imports

--- external_imported/vcpkg/ports/discordcoreapi/portfile.cmake
+++ external_imported/vcpkg/ports/discordcoreapi/portfile.cmake
@@ -1,12 +1,12 @@
 if(VCPKG_TARGET_IS_LINUX)
-    message(WARNING "Discordcoreapi only supports g++ 11 on linux.")
+    message(WARNING "DiscordCoreAPI only supports g++ 11 on linux.")
 endif()
 
 vcpkg_from_github(
 	OUT_SOURCE_PATH SOURCE_PATH
 	REPO RealTimeChris/DiscordCoreAPI
-	REF 6d7c899f0690513855119aa225259dccf17b10f0
-	SHA512 ff42ed8ae799b8f875158842c47fcb5612a5b8861dbfb442c97ab131d952ed59140b2d2431a0d389aefbecb122263f340ff9c8fb863466d0aa91875f8080616e
+	REF 057df9c13edce1939e48abb0ecbf737bd8349bd2
+	SHA512 81ec37163c7667e3f87b91db95f71f52d3a9301200131626e67fae1d23a774edf6e0597d987a7a7cc4128419a147da0766186e9ee5a14efafc5751c8de31ca27
 	HEAD_REF main
 )
 
@@ -18,18 +18,14 @@ vcpkg_cmake_install()
 
 vcpkg_cmake_config_fixup(NO_PREFIX_CORRECTION)
 
-vcpkg_fixup_pkgconfig()
-
-vcpkg_copy_pdbs()
-
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include" "${CURRENT_PACKAGES_DIR}/debug/share")
 
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
     file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
 endif()
 
 file(
-	INSTALL "${SOURCE_PATH}/License"
+	INSTALL "${SOURCE_PATH}/License.md"
 	DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
 	RENAME copyright
 )
