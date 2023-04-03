f1de934b06dd868f6e32a846f178f4feb7933abd
Jason Gauci <jgmath2000@gmail.com>
2023-04-03 12:19:43 -0500

    fix cxxopts

--- test/TestHeaders.hpp
+++ test/TestHeaders.hpp
@@ -1,11 +1,12 @@
 #ifndef __TEST_HEADERS_HPP__
 #define __TEST_HEADERS_HPP__
 
+#include "Headers.hpp"
+
+#undef CHECK
 #include <catch2/benchmark/catch_benchmark.hpp>
 #include <catch2/catch_session.hpp>
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/matchers/catch_matchers_all.hpp>
-
-#include "Headers.hpp"
 
 #endif
