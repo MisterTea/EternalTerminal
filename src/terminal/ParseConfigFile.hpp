4047ffc560decbf34c995d7342b997c3cc2c3b0b
Gabriel Russo <gabriel.russo@databricks.com>
2025-03-06 16:00:39 +0100

    Fix parsing bug where includes are skipped

--- src/terminal/ParseConfigFile.hpp
+++ src/terminal/ParseConfigFile.hpp
@@ -1245,7 +1245,7 @@ static int ssh_config_parse_line(const char *targethost,
     case SOC_INCLUDE: /* recursive include of other files */
 
       p = ssh_config_get_str_tok(&s, NULL);
-      if (p && *parsing) {
+      if (p) {
         char *filename = ssh_path_expand_tilde(p);
         if (filename) {
           local_parse_file(targethost, options, filename, parsing, seen);
