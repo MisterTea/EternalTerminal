5673969b7c04d15111dcd330710ae31ba0c33499
Miaojiang (MJ) Deng <mjdeng@meta.com>
2026-09-17 18:34:15 -0700

    Preserve partial socket write progress (#815)
    Thanks!  Give my best to the people at Meta!

--- test/unit_tests/UnixSocketHandlerTest.cpp
+++ test/unit_tests/UnixSocketHandlerTest.cpp
@@ -3,6 +3,154 @@
 
 using namespace et;
 
+#ifndef WIN32
+namespace {
+class TestPipeSocketHandler : public PipeSocketHandler {
+ public:
+  void trackSocket(int fd) {
+    addToActiveSockets(fd);
+    initSocket(fd);
+  }
+};
+}  // namespace
+
+TEST_CASE("WriteReturnsCommittedPrefixOnTimeout", "[UnixSocketHandler]") {
+  int sockets[2];
+  REQUIRE(::socketpair(AF_UNIX, SOCK_STREAM, 0, sockets) == 0);
+
+  int sendBufferSize = 4096;
+  REQUIRE(::setsockopt(sockets[0], SOL_SOCKET, SO_SNDBUF, &sendBufferSize,
+                       sizeof(sendBufferSize)) == 0);
+
+  TestPipeSocketHandler socketHandler;
+  socketHandler.trackSocket(sockets[0]);
+
+  const string filler(4096, 'f');
+  size_t fillerBytes = 0;
+  while (true) {
+    const ssize_t result = ::send(sockets[0], filler.data(), filler.size(), 0);
+    if (result > 0) {
+      fillerBytes += result;
+      continue;
+    }
+    REQUIRE(result == -1);
+    REQUIRE((GetErrno() == EAGAIN || GetErrno() == EWOULDBLOCK));
+    break;
+  }
+
+  array<char, 4096> receiveBuffer;
+  size_t drainedFillerBytes = 0;
+  pollfd writable{.fd = sockets[0], .events = POLLOUT, .revents = 0};
+  do {
+    const ssize_t result =
+        ::recv(sockets[1], receiveBuffer.data(), receiveBuffer.size(), 0);
+    REQUIRE(result > 0);
+    drainedFillerBytes += result;
+    writable.revents = 0;
+    REQUIRE(::poll(&writable, 1, 0) >= 0);
+  } while ((writable.revents & POLLOUT) == 0);
+
+  const string payload(fillerBytes * 2, 'p');
+  const ssize_t writeResult =
+      socketHandler.write(sockets[0], payload.data(), payload.size());
+
+  const int peerFlags = ::fcntl(sockets[1], F_GETFL);
+  REQUIRE(peerFlags >= 0);
+  REQUIRE(::fcntl(sockets[1], F_SETFL, peerFlags | O_NONBLOCK) == 0);
+
+  vector<char> queuedBytes;
+  while (true) {
+    const ssize_t result =
+        ::recv(sockets[1], receiveBuffer.data(), receiveBuffer.size(), 0);
+    if (result > 0) {
+      queuedBytes.insert(queuedBytes.end(), receiveBuffer.begin(),
+                         receiveBuffer.begin() + result);
+      continue;
+    }
+    REQUIRE(result == -1);
+    REQUIRE((GetErrno() == EAGAIN || GetErrno() == EWOULDBLOCK));
+    break;
+  }
+
+  const size_t remainingFillerBytes = fillerBytes - drainedFillerBytes;
+  REQUIRE(queuedBytes.size() > remainingFillerBytes);
+  const size_t committedPayloadBytes =
+      queuedBytes.size() - remainingFillerBytes;
+  REQUIRE(std::all_of(queuedBytes.begin(),
+                      queuedBytes.begin() + remainingFillerBytes,
+                      [](char byte) { return byte == 'f'; }));
+  REQUIRE(std::all_of(queuedBytes.begin() + remainingFillerBytes,
+                      queuedBytes.end(),
+                      [](char byte) { return byte == 'p'; }));
+
+  socketHandler.close(sockets[0]);
+  REQUIRE(::close(sockets[1]) == 0);
+
+  REQUIRE(committedPayloadBytes > 0);
+  REQUIRE(committedPayloadBytes < payload.size());
+  REQUIRE(writeResult == static_cast<ssize_t>(committedPayloadBytes));
+}
+
+TEST_CASE("WriteReturnsCommittedPrefixOnHardError", "[UnixSocketHandler]") {
+  int sockets[2];
+  REQUIRE(::socketpair(AF_UNIX, SOCK_STREAM, 0, sockets) == 0);
+
+  int sendBufferSize = 4096;
+  REQUIRE(::setsockopt(sockets[0], SOL_SOCKET, SO_SNDBUF, &sendBufferSize,
+                       sizeof(sendBufferSize)) == 0);
+
+  TestPipeSocketHandler socketHandler;
+  socketHandler.trackSocket(sockets[0]);
+
+  const string payload(1024 * 1024, 'p');
+  ssize_t writeResult = 0;
+  int writeErrno = 0;
+  thread writer([&]() {
+    writeResult =
+        socketHandler.write(sockets[0], payload.data(), payload.size());
+    writeErrno = GetErrno();
+  });
+
+  array<char, 4096> receiveBuffer;
+  vector<char> receivedBytes;
+  pollfd readable{.fd = sockets[1], .events = POLLIN, .revents = 0};
+  const int pollResult = ::poll(&readable, 1, 5000);
+  ssize_t firstRead = -1;
+  if (pollResult == 1) {
+    firstRead =
+        ::recv(sockets[1], receiveBuffer.data(), receiveBuffer.size(), 0);
+  }
+  if (firstRead > 0) {
+    receivedBytes.insert(receivedBytes.end(), receiveBuffer.begin(),
+                         receiveBuffer.begin() + firstRead);
+  }
+
+  const int shutdownResult = ::shutdown(sockets[0], SHUT_WR);
+  writer.join();
+
+  socketHandler.close(sockets[0]);
+  ssize_t readResult;
+  while ((readResult = ::recv(sockets[1], receiveBuffer.data(),
+                              receiveBuffer.size(), 0)) > 0) {
+    receivedBytes.insert(receivedBytes.end(), receiveBuffer.begin(),
+                         receiveBuffer.begin() + readResult);
+  }
+
+  const int closeResult = ::close(sockets[1]);
+
+  REQUIRE(pollResult == 1);
+  REQUIRE(firstRead > 0);
+  REQUIRE(shutdownResult == 0);
+  REQUIRE(readResult == 0);
+  REQUIRE(closeResult == 0);
+  REQUIRE(writeErrno == EPIPE);
+  REQUIRE(receivedBytes.size() < payload.size());
+  REQUIRE(
+      std::equal(receivedBytes.begin(), receivedBytes.end(), payload.begin()));
+  REQUIRE(writeResult == static_cast<ssize_t>(receivedBytes.size()));
+}
+#endif
+
 TEST_CASE("AcceptTransientErrorClassification", "[UnixSocketHandler]") {
   // The errnos that must be tolerated rather than aborting the server.
   // ECONNABORTED is the case that crashed etserver on FreeBSD.
